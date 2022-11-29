47a1d979bcc58bfd89140d3a0a509d5987b3eddb
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-30 01:52:16 +0530

    Add createNote form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import type { Component } from 'solid-js';
+import CreateNote from './components/CreateNote/CreateNote';
 import Footer from './components/Layout/Footer/Footer';
 import Header from './components/Layout/Header/Header';
 
 const App: Component = () => {
   return (<>
     <Header />
+    <main>
+      <CreateNote />
+    </main>
     <Footer />
   </>
   );
---
+++ src/components/CreateNote/CreateNote.tsx
@@ -0,0 +1,10 @@
+import { createSignal } from "solid-js"
+
+export default function() {
+  return <div>
+    <form>
+      <input placeholder="Title" />
+      <input placeholder="Take a note..."/>
+    </form>
+  </div>
+}
