81defb16dfd87c24743245d1c3a5201f26a2e03b
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-30 19:19:13 +0530

    Add refs for tracking input value

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+export {};
+
+declare global {
+	type Refs<T> = T | undefined;
+}
--- src/components/CreateNote/CreateNote.tsx
+++ src/components/CreateNote/CreateNote.tsx
@@ -1,10 +1,23 @@
-import { createSignal } from "solid-js"
+import { createSignal, Ref } from "solid-js";
 
-export default function() {
-  return <div>
-    <form>
-      <input placeholder="Title" />
-      <input placeholder="Take a note..."/>
-    </form>
-  </div>
+export default function () {
+	const [focused, setFocused] = createSignal(false);
+
+	let titleRef: Refs<HTMLInputElement>, contentRef: Refs<HTMLInputElement>;
+
+	const handleFocusIn = () => {
+		setFocused(true);
+	};
+
+	const handleFocusOut = () => {
+		setFocused(false);
+		console.log(contentRef?.value)
+	};
+
+	return (
+		<form onFocusOut={handleFocusOut} onFocusIn={handleFocusIn}>
+			{focused() && <input ref={titleRef} />}
+			<input ref={contentRef} />
+		</form>
+	);
 }
