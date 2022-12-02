73bc2f08e378a320656763cbfd6173ed7c19e942
TanishqSingla <tanishqsingla00@gmail.com>
2022-12-03 02:50:23 +0530

    Add reset div function to reset the form

--- src/components/CreateNote/CreateNote.tsx
+++ src/components/CreateNote/CreateNote.tsx
@@ -1,29 +1,49 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 
 export default function () {
 	const [focused, setFocused] = createSignal(false);
 
 	let titleRef: Refs<HTMLDivElement>, contentRef: Refs<HTMLDivElement>;
 
+	const resetDiv = () => {
+		setFocused(false);
+		titleRef!.innerHTML = "";
+		contentRef!.innerHTML = "";
+	};
+
 	const handleFocusOut = (e: FocusEvent) => {
 		const relatedTarget = e.relatedTarget;
-		const eventTarget = e.target as HTMLDivElement 
+		const eventTarget = e.target as HTMLDivElement;
 		if (relatedTarget) return;
 		if (e.target === document.activeElement) return;
-		if(!eventTarget.textContent) {
-			eventTarget.innerHTML = '';
+		if (!eventTarget.textContent) {
+			eventTarget.innerHTML = "";
 		}
-		setFocused(false);
-	}
-
-	const handleFocusIn = (e: FocusEvent) => {
-		setFocused(true);
-	}
+		console.log(eventTarget.innerText);
+		resetDiv();
+	};
 
 	return (
-		<div class="createNote" onFocusOut={handleFocusOut} onFocusIn={handleFocusIn}>
-			{focused() && <div><div contentEditable ref={titleRef} class="titleInput input" aria-label="Title" /></div>}
-			<div><div contentEditable ref={contentRef} class="contentInput input" aria-label="Take a note..." /></div>
+		<div class="createNote" onFocusOut={handleFocusOut}>
+			{focused() && (
+				<div>
+					<div
+						contentEditable
+						ref={titleRef}
+						class="titleInput input"
+						aria-label="Title"
+					/>
+				</div>
+			)}
+			<div>
+				<div
+					contentEditable
+					ref={contentRef}
+					onFocus={() => setFocused(true)}
+					class="contentInput input"
+					aria-label="Take a note..."
+				/>
+			</div>
 		</div>
 	);
 }
